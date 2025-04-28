90f3b4afa5436d470b1441e1f51f7c89ac2ea6fd
LanderDebeir <lander.debeir@student.howest.be>
2025-04-28 11:21:19 +0200

    Create movie event class

--- src/Howestprime.Movies.Domain/MovieEvent/MovieEvent.cs
+++ src/Howestprime.Movies.Domain/MovieEvent/MovieEvent.cs
@@ -1,5 +1,59 @@
 ﻿using Domaincrafters.Domain;
+using Howestprime.Movies.Domain.Movie;
 
 namespace Howestprime.Movies.Domain.MovieEvent;
 
 public sealed class MovieEventId(string? id = ""): UuidEntityId(id);
+
+public class MovieEvent : Entity<MovieEventId>
+{
+    public MovieId MovieId { get; private set; }
+    public RoomId RoomId { get; private set; }
+    public DateTime Time { get; private set; }
+    public int Visitors { get; private set; }
+    public int Capacity { get; private set; }
+    public IReadOnlyList<Booking> Bookings { get; private set; }
+
+    private MovieEvent(MovieEventId id, MovieId movieId, RoomId roomId, DateTime time, int visitors, int capacity) : base(id)
+    {
+        MovieId = movieId;
+        RoomId = roomId;
+        Time = time;
+        Visitors = visitors;
+        Capacity = capacity;
+        Bookings = new List<Booking>();
+    }
+
+    public static MovieEvent Create(MovieId movieId, RoomId roomId, DateTime time, int visitors, int capacity, MovieEventId? id = null)
+    {
+        id ??= new MovieEventId();
+
+        MovieEvent movieEvent = new MovieEvent(id, movieId, roomId, time, visitors, capacity);
+
+        movieEvent.ValidateState();
+
+        return movieEvent;
+    }
+
+
+    public override void ValidateState()
+    {
+        throw new NotImplementedException();
+    }
+
+    public static void EnsureValidVisitors(int visitors)
+    {
+        if (int.IsNegative(visitors))
+        {
+            throw new ArgumentException("Amount of visitors has to be positive");
+        }
+    }
+
+    public static EnsureValidCapacity(int capacity)
+    {
+        if (int.IsNegative(capacity))
+        {
+            throw new ArgumentException("Capacity has to be positive");
+        }
+    }
+}
