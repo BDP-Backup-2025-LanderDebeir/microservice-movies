faae0bb8a2dff9df4f967ed8c83e0fd491b1824c
LanderDebeir <lander.debeir@student.howest.be>
2025-04-29 08:56:36 +0200

    Add movie data

---
+++ src/Howestprime.Movies.Application/Contracts/Data/MovieData.cs
@@ -0,0 +1,14 @@
+﻿namespace Howestprime.Movies.Application.Contracts.Data;
+
+public sealed record MovieData
+{
+    public required string Id { get; set; }
+    public required string PosterUrl { get; set; }
+    public required string Title { get; set; }
+    public required string Genre { get; set; }
+    public required int AgeRating { get; set; }
+    public required int Year { get; set; }
+    public required int Duration { get; set; }
+    public required string Actors { get; set; }
+    public required string Description { get; set; }
+}
