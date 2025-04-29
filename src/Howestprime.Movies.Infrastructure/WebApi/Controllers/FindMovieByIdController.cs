913ad40a3054e07d5f350e044467ccdb9f74d080
LanderDebeir <lander.debeir@student.howest.be>
2025-04-29 12:13:41 +0200

    Create find movie by id controller

---
+++ src/Howestprime.Movies.Infrastructure/WebApi/Controllers/FindMovieByIdController.cs
@@ -0,0 +1,48 @@
+﻿using Domaincrafters.Application;
+using Howestprime.Movies.Application.Contracts.Data;
+using Howestprime.Movies.Application.Movies;
+using Howestprime.Movies.Infrastructure.WebApi.Controllers.Responses;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Http.HttpResults;
+using Microsoft.AspNetCore.Mvc;
+using RabbitMQ.Client;
+using System.ComponentModel.DataAnnotations;
+
+namespace Howestprime.Movies.Infrastructure.WebApi.Controllers;
+
+public sealed record FindMovieByIdParameters
+{
+    [Required]
+    public required string Id { get; set; }
+}
+
+public sealed class FindMovieByIdController
+{
+    public static async Task<Results<Ok<MovieDTO>, NotFound>> Invoke(
+        [AsParameters] FindMovieByIdParameters parameters,
+        [FromServices] IUseCase<FindMovieByIdInput, Task<MovieData>> findMovieById
+        )
+    {
+        FindMovieByIdInput input = new(parameters.Id);
+        MovieData movie = await findMovieById.Execute(input);
+
+        return TypedResults.Ok(BuildResponse(movie));
+    }
+
+    private static MovieDTO BuildResponse(MovieData movie)
+    {
+        return new MovieDTO(
+            movie.Id,
+            movie.Title,
+            movie.Genre,
+            movie.Description,
+            movie.Year,
+            movie.Duration,
+            movie.Actors,
+            movie.AgeRating,
+            movie.PosterUrl
+            );
+
+
+    }
+}
