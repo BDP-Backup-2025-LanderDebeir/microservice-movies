401dcf8b4ff5a995c1c7ba1ed2c8ca3a4483aa97
LanderDebeir <lander.debeir@student.howest.be>
2025-04-29 11:41:49 +0200

    Create find movie by id usecase

---
+++ src/Howestprime.Movies.Application/Movies/FindMovieById.cs
@@ -0,0 +1,22 @@
+﻿using Domaincrafters.Application;
+using Howestprime.Movies.Application.Contracts.Data;
+using Howestprime.Movies.Application.Contracts.Ports;
+using System.Threading.Tasks;
+
+namespace Howestprime.Movies.Application.Movies;
+
+public sealed record FindMovieByIdInput(
+    string Id
+    );
+
+public class FindMovieById(
+    IFindMovieByIdQuery query
+    ) : IUseCase<FindMovieByIdInput, Task<MovieData>>
+{
+    private readonly IFindMovieByIdQuery _query = query;
+
+    public async Task<MovieData> Execute(FindMovieByIdInput input)
+    {
+        return await _query.Fetch(input.Id) ?? throw new InvalidOperationException($"Movie with id ${input.Id} not found");
+    }
+}
