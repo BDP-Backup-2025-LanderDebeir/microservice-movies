21a2f36a3da54969585a988fd10137176dc2a756
LanderDebeir <lander.debeir@student.howest.be>
2025-04-29 09:47:42 +0200

    Register find movie by filter for dependency injection

--- src/Howestprime.Movies.Main/Modules/UseCaseServices.cs
+++ src/Howestprime.Movies.Main/Modules/UseCaseServices.cs
@@ -1,4 +1,6 @@
 using Domaincrafters.Application;
+using Howestprime.Movies.Application.Contracts.Data;
+using Howestprime.Movies.Application.Contracts.Ports;
 using Howestprime.Movies.Application.Movies;
 using Howestprime.Movies.Domain.Movie;
 
@@ -9,7 +11,8 @@ public static class UseCaseServices
     public static IServiceCollection AddUseCases(this IServiceCollection services)
     {
         return services
-            .AddRegisterMovie();
+            .AddRegisterMovie()
+            .AddFindMovieByFilter();
     }
 
     private static IServiceCollection AddRegisterMovie(this IServiceCollection services)
@@ -22,6 +25,16 @@ public static class UseCaseServices
                 var logger = ServiceProvider.GetRequiredService<ILogger<RegisterMovie>>();
 
                 return new RegisterMovie(movieRepository, unitOfWork, logger);
+            });
+    }
+
+    private static IServiceCollection AddFindMovieByFilter(this IServiceCollection services)
+    {
+        return services
+            .AddScoped<IUseCase<FindMovieByFilterInput, Task<IReadOnlyList<MovieData>>>>(ServiceProvider =>
+            {
+                var query = ServiceProvider.GetRequiredService<IFindMovieQuery>();
+                return new FindMovieByFilter(query);
             });
     }
 }
