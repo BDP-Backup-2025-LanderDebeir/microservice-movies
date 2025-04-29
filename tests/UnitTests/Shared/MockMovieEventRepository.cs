6e7dedfd938ec078253f95a60d6d066f6ddc72c5
LanderDebeir <lander.debeir@student.howest.be>
2025-04-29 16:32:28 +0200

    Create mock movie event repository

---
+++ tests/UnitTests/Shared/MockMovieEventRepository.cs
@@ -0,0 +1,39 @@
+﻿using Aornis;
+using Howestprime.Movies.Domain.MovieEvent;
+
+namespace UnitTests.Shared;
+
+public sealed class MockMovieEventRepository : IMovieEventRepository
+{
+    public List<MovieEvent> SavedMovieEvents { get; } = new List<MovieEvent>();
+    public List<MovieEvent> MovieEvents { get; } = new List<MovieEvent>();
+    public Exception? ThrowOnSave { get; set; }
+
+    public Task<Optional<MovieEvent>> ById(MovieEventId id)
+    {
+        var movieEvent = MovieEvents.SingleOrDefault(m => m.Id == id);
+        return Task.FromResult(Optional.Of<MovieEvent>(movieEvent));
+    }
+
+    public Task Remove(MovieEvent entity)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task Save(MovieEvent entity)
+    {
+        if (ThrowOnSave != null)
+            throw ThrowOnSave;
+
+        SavedMovieEvents.Add(entity);
+
+        var existingIndex = MovieEvents.FindIndex(m => m.Id == entity.Id);
+
+        if (existingIndex >= 0)
+            MovieEvents[existingIndex] = entity;
+        else
+            MovieEvents.Add(entity);
+
+        return Task.CompletedTask;
+    }
+}
