1fd9e158086a548cc65369b3fa4474d49b4c5d4f
LanderDebeir <lander.debeir@student.howest.be>
2025-04-30 09:52:14 +0200

    Implement find movie by id with events query in entity framework

---
+++ src/Howestprime.Movies.Infrastructure/Persistence/EntityFramework/Queries/EFFindMovieByIdWithEventsQuery.cs
@@ -0,0 +1,20 @@
+﻿using Howestprime.Movies.Application.Contracts.Data;
+using Howestprime.Movies.Application.Contracts.Ports;
+using Howestprime.Movies.Infrastructure.Persistence.EntityFramework.Configurations;
+using Microsoft.EntityFrameworkCore;
+
+namespace Howestprime.Movies.Infrastructure.Persistence.EntityFramework.Queries;
+
+public sealed class EFFindMovieByIdWithEventsQuery(
+    QueryContextBase context
+    ) : IFindMovieByIdWithEventsQuery
+{
+    private readonly QueryContextBase _context = context;
+
+    public async Task<MovieData?> Fetch(string id)
+    {
+        return await _context.Movies
+            .Include(m => m.Events)
+            .FirstOrDefaultAsync(m => m.Id == id);
+    }
+}
