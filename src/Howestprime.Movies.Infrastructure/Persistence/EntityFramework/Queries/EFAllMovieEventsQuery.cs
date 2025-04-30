00a88d56d1d9e1dd0803c99bd06e3790d5ee597c
LanderDebeir <lander.debeir@student.howest.be>
2025-04-30 12:05:50 +0200

    Implement all movie events query with Entity Framework

---
+++ src/Howestprime.Movies.Infrastructure/Persistence/EntityFramework/Queries/EFAllMovieEventsQuery.cs
@@ -0,0 +1,23 @@
+﻿using Howestprime.Movies.Application.Contracts.Data;
+using Howestprime.Movies.Application.Contracts.Ports;
+using Howestprime.Movies.Infrastructure.Persistence.EntityFramework.Configurations;
+using Microsoft.EntityFrameworkCore;
+using System.Linq.Expressions;
+
+namespace Howestprime.Movies.Infrastructure.Persistence.EntityFramework.Queries;
+
+public sealed class EFAllMovieEventsQuery(
+    QueryContextBase context
+    ) : IAllMovieEventsQuery
+{
+    private readonly QueryContextBase _context = context;
+
+    public async Task<IReadOnlyList<MovieEventData>> Fetch(Expression<Func<MovieEventData, bool>> filter)
+    {
+        return await _context.MovieEvents
+            .Where(filter)
+            .Include(e => e.Movie)
+            .Include(e => e.Room)
+            .ToListAsync();
+    }
+}
