a3bc6b6782c342ad8102858817a26cceb1a7f011
LanderDebeir <lander.debeir@student.howest.be>
2025-04-30 09:18:18 +0200

    Create room data

---
+++ src/Howestprime.Movies.Application/Contracts/Data/RoomData.cs
@@ -0,0 +1,8 @@
+﻿namespace Howestprime.Movies.Application.Contracts.Data;
+
+public sealed record RoomData
+{
+    public required string Id { get; set; }
+    public required string Name { get; set; }
+    public required int Capacity { get; set; }
+}
