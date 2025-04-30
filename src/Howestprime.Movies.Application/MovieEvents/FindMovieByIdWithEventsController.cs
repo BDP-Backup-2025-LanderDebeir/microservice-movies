800771b21ec04e31fbe62b11f387890c5e21b4cb
LanderDebeir <lander.debeir@student.howest.be>
2025-04-30 09:47:06 +0200

    Create find movie by id with events use case

---
+++ src/Howestprime.Movies.Application/MovieEvents/FindMovieByIdWithEventsController.cs
@@ -0,0 +1,22 @@
+﻿using Domaincrafters.Application;
+using Howestprime.Movies.Application.Contracts.Data;
+using Howestprime.Movies.Application.Contracts.Ports;
+using Howestprime.Movies.Domain.Shared.Exceptions;
+
+namespace Howestprime.Movies.Application.MovieEvents;
+
+public sealed record FindMovieByIdWithEventsInput(
+    string MovieId
+    );
+
+public class FindMovieByIdWithEvents(
+    IFindMovieByIdWithEventsQuery query
+    ) : IUseCase<FindMovieByIdWithEventsInput, Task<MovieData>>
+{
+    public IFindMovieByIdWithEventsQuery _query = query;
+
+    public async Task<MovieData> Execute(FindMovieByIdWithEventsInput input)
+    {
+        return await _query.Fetch(input.MovieId) ?? throw new NotFoundException($"Movie with id {input.MovieId} not found");
+    }
+}
