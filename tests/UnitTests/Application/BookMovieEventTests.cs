e3bd99b6662e83b8cb199581dab5065ada0fe4d1
LanderDebeir <lander.debeir@student.howest.be>
2025-05-01 15:15:52 +0200

    Create tests for book movie event use case

---
+++ tests/UnitTests/Application/BookMovieEventTests.cs
@@ -0,0 +1,62 @@
+﻿using Howestprime.Movies.Application.MovieEvents;
+using Howestprime.Movies.Domain.MovieEvent;
+using UnitTests.Shared;
+
+namespace UnitTests.Application;
+
+public sealed class BookMovieEventTests
+{
+    private readonly MockMovieEventRepository _repository;
+    private readonly MockUnitOfWork _unitOfWork;
+    private readonly BookMovieEvent _bookMovieEvent;
+
+    public BookMovieEventTests()
+    {
+        _repository = new();
+        _unitOfWork = new();
+        _bookMovieEvent = new(_repository, _unitOfWork);
+    }
+
+    [Fact]
+    public async Task Execute_WithValidData_ShouldAddBooking()
+    {
+        //Arrange
+        MovieEventId movieEventId = new();
+        MovieEvent movieEvent = MovieEvent.Create(new(), new(), new(2025, 5, 10, 15, 0, 0), 100, movieEventId);
+        await _repository.Save(movieEvent);
+        BookMovieEventInput input = new(movieEventId.Value, 1, 0);
+
+        //Act
+        var result = await _bookMovieEvent.Execute(input);
+        Assert.Single(movieEvent.Bookings);
+        var booking = movieEvent.Bookings[0];
+
+        Assert.Equal(result, booking.Id.Value);
+
+        Assert.Equal(1, movieEvent.Visitors);
+    }
+
+    [Fact]
+    public void Execute_WithNonExistingEvent_ShouldThrowError()
+    {
+        //Arrange
+        BookMovieEventInput input = new(new MovieEventId().Value, 1, 0);
+
+        //Act + Assert
+        _ = Assert.ThrowsAsync<InvalidOperationException>(async () => await _bookMovieEvent.Execute(input));
+    }
+
+    [Fact]
+    public async Task Execute_WithEventInTheFarFuture_ShouldThrowErrorAsync()
+    {
+        //Arrange
+        MovieEventId movieEventId = new();
+        MovieEvent movieEvent = MovieEvent.Create(new(), new(), new(2099, 5, 10, 15, 0, 0), 100, movieEventId);
+        await _repository.Save(movieEvent);
+        BookMovieEventInput input = new(movieEventId.Value, 1, 0);
+
+        //Act + Assert
+        var result = await Assert.ThrowsAsync<InvalidOperationException>(async () => await _bookMovieEvent.Execute(input));
+        Assert.Equal($"Bookings for the event with id {movieEventId} haven't opened yet", result.Message);
+    }
+}
