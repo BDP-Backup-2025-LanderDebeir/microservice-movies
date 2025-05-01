3a79dbb383fcac9a2f5c8c022ebc182ed5876411
LanderDebeir <lander.debeir@student.howest.be>
2025-05-01 15:29:40 +0200

    Create book movie event controller

---
+++ src/Howestprime.Movies.Infrastructure/WebApi/Controllers/BookMovieEventController.cs
@@ -0,0 +1,35 @@
+﻿using Domaincrafters.Application;
+using Howestprime.Movies.Application.MovieEvents;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Http.HttpResults;
+using Microsoft.AspNetCore.Mvc;
+using System.ComponentModel.DataAnnotations;
+
+namespace Howestprime.Movies.Infrastructure.WebApi.Controllers;
+
+public sealed record BookMovieEventBody
+{
+    [Required]
+    public required int StandardVisitors { get; set; }
+
+    [Required]
+    public required int DiscountVisitors { get; set; }
+}
+
+public sealed class BookMovieEventController
+{
+    public static async Task<Results<Created, BadRequest>> Invoke(
+        [FromBody] BookMovieEventBody body,
+        [AsParameters] Guid movieEventId,
+        [FromServices] IUseCase<BookMovieEventInput, Task<string>> bookMovieEvent)
+    {
+        BookMovieEventInput input = new(
+            movieEventId.ToString(),
+            body.StandardVisitors,
+            body.DiscountVisitors
+        );
+
+        string bookingId = await bookMovieEvent.Execute(input);
+        return TypedResults.Created($"/movie-events/{movieEventId}/bookings/{bookingId}");
+    }
+}
